e3f449d645b09bc46f3aa4a3eb55316a23af5d98
Nick Babcock <nbabcock19@hotmail.com>
2015-02-26 14:46:00 -0500

    Init commit

---
+++ tests/Pfarah.Tests/Tests.fs
@@ -0,0 +1,10 @@
+module Pfarah.Tests
+
+open Pfarah
+open NUnit.Framework
+
+[<Test>]
+let ``hello returns 42`` () =
+  let result = Library.hello 42
+  printfn "%i" result
+  Assert.AreEqual(42,result)
