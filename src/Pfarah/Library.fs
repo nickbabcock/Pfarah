e4823d468140440995e85046857cedcc281c76b8
Nick Babcock <nbabcock19@hotmail.com>
2015-02-26 16:48:26 -0500

    Start of parsing

--- src/Pfarah/Library.fs
+++ src/Pfarah/Library.fs
@@ -1,5 +1,3 @@
-namespace Pfarah
-
 /// Documentation for my library
 ///
 /// ## Example
@@ -7,10 +5,28 @@ namespace Pfarah
 ///     let h = Library.hello 1
 ///     printfn "%d" h
 ///
-module Library = 
-  
-  /// Returns 42
-  ///
-  /// ## Parameters
-  ///  - `num` - whatever
-  let hello num = 42
+module Pfarah
+
+open System
+open System.IO
+open System.Text
+
+type PfData =
+  | Pfbool of bool
+  | Pfnumber of float
+  | Pfdate of DateTime
+  | Pfstring of string
+  | Pflist of PfData list
+  | PfObj of Map<string, PfData>
+
+let isspace (c:int) = c = 10 || c = 13 || c = 11 || c = 32
+
+let skipWhitespace (stream:StreamReader) =
+  while (isspace (stream.Peek())) do
+    stream.Read() |> ignore
+
+let parse file () =
+  use fs = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.ReadWrite, 0x8000)
+  use stream = new StreamReader(fs, Encoding.GetEncoding(1252), false, 0x8000)
+  skipWhitespace stream
+  fs
