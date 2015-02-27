ecedc1376b000424bacf19a8f31030d98a762f30
Nick Babcock <nbabcock19@hotmail.com>
2015-02-27 09:42:32 -0500

    First actual test

--- tests/Pfarah.Tests/Tests.fs
+++ tests/Pfarah.Tests/Tests.fs
@@ -4,7 +4,5 @@ open Pfarah
 open NUnit.Framework
 
 [<Test>]
-let ``hello returns 42`` () =
-  let result = Library.hello 42
-  printfn "%i" result
-  Assert.AreEqual(42,result)
+let ``a space is whitespace`` () =
+  Assert.AreEqual(Pfarah.isspace (int ' '), true)
