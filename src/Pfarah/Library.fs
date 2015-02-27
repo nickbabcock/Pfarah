6c381899b28304d9a0e0d2284eb621493896d63f
Nick Babcock <nbabcock19@hotmail.com>
2015-02-27 18:05:38 -0500

    More parsing structure

--- src/Pfarah/Library.fs
+++ src/Pfarah/Library.fs
@@ -19,14 +19,71 @@ type PfData =
   | Pflist of PfData list
   | PfObj of Map<string, PfData>
 
+
 let isspace (c:int) = c = 10 || c = 13 || c = 9 || c = 32
 
 let skipWhitespace (stream:StreamReader) =
   while (isspace (stream.Peek())) do
     stream.Read() |> ignore
 
+let tryDate (str:string) =
+  match str.Split('.') with
+  | [|y;m;d|] -> Some(new DateTime(int y, int m, int d))
+  | [|y;m;d;h|] -> Some(new DateTime(int y, int m, int d, int h, 0, 0))
+  | _ -> None
+
+let isnum (c:char) =
+  c >= '0' && c <= '9'
+
+type ParaParser (stream:StreamReader) =
+  let MaxTokenSize = 256
+  let (stringBuffer:char[]) = [||]
+  let mutable stringBufferCount = 0
+  let obj = PfObj Map.empty
+
+  member self.readString () =
+    let mutable isDone = false
+    while isDone <> true do
+      let next = stream.Peek()
+      isDone <- isspace next || next = 61 || next = -1
+      if not (isDone) then
+        stringBuffer.[stringBufferCount] <- (char (stream.Read()))
+        stringBufferCount <- stringBufferCount + 1
+
+    let result = new String(stringBuffer, 0, stringBufferCount)
+    stringBufferCount <- 0
+    result
+
+  member self.quotedStringRead() =
+    while stream.Peek() <> 34 do
+      stringBuffer.[stringBufferCount] <- (char (stream.Read()))
+      stringBufferCount <- stringBufferCount + 1
+    let result = new String(stringBuffer, 0, stringBufferCount)
+    stringBufferCount <- 0
+    result
+
+  member self.Parse () =
+    skipWhitespace stream
+    let key = self.readString ()
+    skipWhitespace stream
+
+    // ASSERT (stream.Peek() = 61)
+    stream.Read() |> ignore
+    skipWhitespace stream
+    let value =
+      match stream.Peek() with
+      | 34 ->
+        let q = self.quotedStringRead()
+        match tryDate q with
+        | Some(date) -> Pfdate date
+        | None -> Pfstring q
+      | 123 -> PfObj Map.empty
+      | _ -> Pfnumber 1.0
+
+    obj
+
 let parse file () =
   use fs = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.ReadWrite, 0x8000)
   use stream = new StreamReader(fs, Encoding.GetEncoding(1252), false, 0x8000)
-  skipWhitespace stream
-  fs
+  let parser = ParaParser stream
+  parser.Parse ()
--- tests/Pfarah.Tests/Tests.fs
+++ tests/Pfarah.Tests/Tests.fs
@@ -2,6 +2,7 @@ module Pfarah.Tests
 
 open Pfarah
 open NUnit.Framework
+open System
 
 [<Test>]
 let ``a space is whitespace`` () =
@@ -18,3 +19,27 @@ let ``a carriage return is whitespace`` () =
 [<Test>]
 let ``a tab is whitespace`` () =
   Assert.AreEqual(Pfarah.isspace (int '\t'), true)
+
+[<Test>]
+let ``parse the earliest date`` () =
+  match Pfarah.tryDate "1.1.1" with
+  | Some(date) -> Assert.AreEqual(date, new DateTime(1,1,1))
+  | None -> Assert.Fail "Expected a date for 1.1.1"
+
+[<Test>]
+let ``parse earliest date with hour`` () =
+  match Pfarah.tryDate "1.1.1.1" with
+  | Some(date) -> Assert.AreEqual(date, new DateTime(1,1,1,1,0,0))
+  | None -> Assert.Fail "Expected a date for 1.1.1.1"
+
+[<Test>]
+let ``parse the date`` () =
+  match Pfarah.tryDate "1492.3.2" with
+  | Some(date) -> Assert.AreEqual(date, new DateTime(1492,3,2))
+  | None -> Assert.Fail "Expected a date for 1492.3.2"
+
+[<Test>]
+let ``parse the hour date`` () =
+  match Pfarah.tryDate "1492.3.2.5" with
+  | Some(date) -> Assert.AreEqual(date, new DateTime(1492,3,2,5,0,0))
+  | None -> Assert.Fail "Expected a date for 1492.3.2.5"
