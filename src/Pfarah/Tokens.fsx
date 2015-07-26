18513e30dfb25b5ba0a9b97fec3d897202c42dd3
Nick Babcock <nbabcock19@hotmail.com>
2015-07-26 19:12:41 -0400

    Start of binary token deduction

---
+++ src/Pfarah/Tokens.fsx
@@ -0,0 +1,45 @@
+﻿#I "../../bin"
+#r "Pfarah.dll"
+open Pfarah
+open System
+open System.IO
+
+let txt, bin =
+  match fsi.CommandLineArgs with
+  | [| txt; bin |] -> txt, bin
+  | x -> failwith "<text file> <binary file>"
+
+let txtData = ParaValue.Load(txt, "EU4bin", "EU4txt", lazy(dict([])))
+let binData = ParaValue.Load(bin, "EU4bin", "EU4txt", lazy(dict([])))
+
+let printMappings = Map.iter(fun name token -> printfn "0x%04x, %s" (Int32.Parse(token)) name)
+let extractKeys x y =
+  Seq.zip (x |> asRecord) (y |> asRecord)
+  |> Seq.filter (fun ((xkey, xval), (ykey, yval)) -> xkey <> ykey)
+  |> Seq.map (fun ((xkey, xval), (ykey, yval)) -> xkey, ykey)
+
+let topMap = extractKeys txtData binData |> Map.ofSeq
+
+let binTrade =
+  binData
+  |> tryFind topMap.["trade"]
+  |> Option.get
+ 
+let tradeMap = extractKeys txtData?trade binTrade |> Map.ofSeq
+let binNodes = binTrade |> collect tradeMap.["node"]
+
+Seq.zip (txtData?trade |> collect "node") (binNodes)
+|> Seq.collect (fun (x, y) -> extractKeys x y)
+|> Map.ofSeq
+|> printMappings
+
+Seq.zip (txtData?trade |> collect "node") (binNodes)
+|> Seq.collect (fun (x, y) ->
+  Seq.zip (x |> asRecord) (y |> asRecord)
+  |> Seq.filter (fun ((xkey, xval), (ykey, yval)) -> xkey = ykey)
+  |> Seq.map (fun ((xkey, xval), (ykey, yval)) -> xval, yval))
+|> Seq.collect (fun (x, y) -> extractKeys x y)
+|> Map.ofSeq
+|> printMappings
+
+topMap |> printMappings
