a35a271aced3ab378b4368f5383acd5790d60e8c
Nick Babcock <nbabcock19@hotmail.com>
2015-11-14 18:36:43 -0500

    Document PeekingStream

--- src/Pfarah/PeekingStream.fs
+++ src/Pfarah/PeekingStream.fs
@@ -2,7 +2,13 @@
 
 open System.IO
 
-type PeekingStream(stream:Stream) =
+/// A PeekingStream is a simpler wrapper around a Stream object that allows
+/// for efficient peeking. The peeking mechanism is somewhat similar to a
+/// StreamReader, except that a PeekingStream keeps the next byte cached so
+/// that repeating calls to Peek only result in simple return instead of an
+/// array lookup. This class was inspired by RapidJson's [FileReadStream]
+/// (https://github.com/miloyip/rapidjson/blob/b39a8982699aed269a8db2c0d1319bd7ab04485a/include/rapidjson/filereadstream.h)
+type PeekingStream (stream:Stream) =
   let mutable current = 0;
   let mutable eof = false
 
