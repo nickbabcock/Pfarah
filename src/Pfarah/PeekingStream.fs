ca2b8b1e72d9006fd65566d689825f9a11608045
Nick Babcock <nbabcock19@hotmail.com>
2015-11-14 18:37:36 -0500

    Keep PeekingStream internal to Pfarah

--- src/Pfarah/PeekingStream.fs
+++ src/Pfarah/PeekingStream.fs
@@ -8,7 +8,7 @@ open System.IO
 /// that repeating calls to Peek only result in simple return instead of an
 /// array lookup. This class was inspired by RapidJson's [FileReadStream]
 /// (https://github.com/miloyip/rapidjson/blob/b39a8982699aed269a8db2c0d1319bd7ab04485a/include/rapidjson/filereadstream.h)
-type PeekingStream (stream:Stream) =
+type internal PeekingStream (stream:Stream) =
   let mutable current = 0;
   let mutable eof = false
 
