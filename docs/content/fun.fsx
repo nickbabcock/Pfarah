0b838966eac9c2bce2a866cb7cfd1ecebc58553e
Nick Babcock <nbabcock19@hotmail.com>
2016-07-10 22:25:00 -0400

    Add interactive F# script

---
+++ docs/content/fun.fsx
@@ -0,0 +1,20 @@
+(*** hide ***)
+// This block of code is omitted in the generated HTML documentation. Use
+// it to define helpers that you do not want to show in the documentation.
+#I "../../bin"
+
+#r "Pfarah.dll"
+open Pfarah
+open Pfarah.Operators
+open System
+
+Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+
+let data = ParaValue.LoadText("../../src/Pfarah.Benchmarks/achievements.txt")
+let trans obj = obj / "possible" / "ironman"
+let ironmen = fmap trans data
+let requireIroman : (string * bool)[] =
+  match ironmen with
+  | ParaValue.Record props ->
+    props |> Array.map (fun (k, v) -> (k, deserialize v))
+  | x -> failwith "Unexpected"
